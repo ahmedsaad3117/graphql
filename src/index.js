96ab01b43340e1bf4dc95d152a76c0fad5b62c39
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-08-28 00:00:34 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
 import { GraphQLServer } from "graphql-yoga";
 
+//Demo Data
+const users = [
+  {
+    id: "1",
+    name: "Ahmed",
+    email: "ahmed@gmail.com",
+    age: 15,
+  },
+  {
+    id: "2",
+    name: "khald",
+    email: "khald@gmail.com",
+  },
+  {
+    id: "1",
+    name: "saad",
+    email: "saas@gmail.com",
+    age: 22,
+  },
+];
+
+const posts = [
+  {
+    id: "10",
+    title: "Post1",
+    body: "this the frist post",
+    published: true,
+  },
+  {
+    id: "12",
+    title: "Post2",
+    body: "this the 2 post",
+    published: true,
+  },
+  {
+    id: "20",
+    title: "Post3",
+    body: "",
+    published: false,
+  },
+];
+
 //Type definions (schema)
 const typeDefs = `
     type Query {
-      greating(name: String) : String!
-      add(a: Number! , b: Number!) : Float!
-       me: User!
-       post: Post!
-       add: Float!
+      users(query: String): [User!]!
+      me: User!
+      posts(query: String): [Post!]!
     }
 
     type User {
@@ -20,36 +60,48 @@ const typeDefs = `
     type Post {
       id: ID!
       title: String!
-      body: String,!
+      body: String!
       published: Boolean!
     }
-
 `;
 
-// Resolvers
+//Resolvers
 const resolvers = {
   Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
     me() {
       return {
         id: "12345",
         name: "Mike",
         email: "mike@example.com",
+        
       };
     },
-    post() {
-      return {
-        id: "12345",
-        title: "frist Post",
-        body: "this my frost post ever",
-        published: true,
-      };
-    },
-    greating(parent, args, ctx) {
-      if (args.name) {
-        return `Hi Mr(s): ${args.name}`;
-      } else {
-        return "Hello!";
+
+    posts(parent, args, ctx, info) {
+      +console.log(args);
+      if (!args.query) {
+        return posts;
       }
+
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+
+        return isBodyMatch || isTitleMatch;
+      });
     },
   },
 };
