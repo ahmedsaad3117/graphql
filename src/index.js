879c10bec21a5246fbf002a1c4e6403d34248583
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-08-29 09:01:51 +0200

    add posts

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const users = [
     email: "khald@gmail.com",
   },
   {
-    id: "1",
+    id: "3",
     name: "saad",
     email: "saas@gmail.com",
     age: 22,
@@ -27,18 +27,48 @@ const posts = [
     title: "Post1",
     body: "this the frist post",
     published: true,
+    author: "1",
   },
   {
     id: "12",
     title: "Post2",
     body: "this the 2 post",
     published: true,
+    author: "1",
   },
   {
     id: "20",
     title: "Post3",
     body: "",
     published: false,
+    author: "2",
+  },
+];
+
+const comments = [
+  {
+    id: "1",
+    text: "comment one",
+    author: "1",
+    post: "10",
+  },
+  {
+    id: "2",
+    text: "comment 2nd",
+    author: "1",
+    post: "10",
+  },
+  {
+    id: "3",
+    text: "comment 3rd",
+    author: "2",
+    post: "20",
+  },
+  {
+    id: "4",
+    text: "comment 4th",
+    author: "2",
+    post: "10",
   },
 ];
 
@@ -48,6 +78,7 @@ const typeDefs = `
       users(query: String): [User!]!
       me: User!
       posts(query: String): [Post!]!
+      comments: [Comment!]!
     }
 
     type User {
@@ -55,6 +86,8 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int  
+        posts: [Post!]!
+        comments: [Comment!]!
     }
 
     type Post {
@@ -62,6 +95,15 @@ const typeDefs = `
       title: String!
       body: String!
       published: Boolean!
+      author: User!
+      comments: [Comment!]!
+    }
+
+    type Comment {
+      id: ID!
+      text: String!
+      author: User!
+      post: Post!
     }
 `;
 
@@ -77,17 +119,20 @@ const resolvers = {
         return user.name.toLowerCase().includes(args.query.toLowerCase());
       });
     },
+
+    comments(parent, args, ctx, info) {
+      return comments;
+    },
+
     me() {
       return {
         id: "12345",
         name: "Mike",
         email: "mike@example.com",
-        
       };
     },
 
     posts(parent, args, ctx, info) {
-      +console.log(args);
       if (!args.query) {
         return posts;
       }
@@ -97,10 +142,48 @@ const resolvers = {
           .toLowerCase()
           .includes(args.query.toLowerCase());
         const isBodyMatch = post.body
+
           .toLowerCase()
           .includes(args.query.toLowerCase());
 
         return isBodyMatch || isTitleMatch;
+      });
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+    comments(parent, args, ctx, info){
+      return comments.filter((comment)=>{
+        return comment.post === parent.id 
+      })
+    }
+
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id;
+      });
+    },
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.autho;
+      });
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => {
+        return post.id === parent.post;
       });
     },
   },
