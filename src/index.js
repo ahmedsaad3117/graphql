d08cd817296834a537a89a5ef82f30dae146c7f2
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-08-29 22:06:15 +0200

    add Mutation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
+import { v4 as uuidv4 } from "uuid";
 
 //Demo Data
 const users = [
@@ -29,6 +30,7 @@ const posts = [
     published: true,
     author: "1",
   },
+
   {
     id: "12",
     title: "Post2",
@@ -36,6 +38,7 @@ const posts = [
     published: true,
     author: "1",
   },
+
   {
     id: "20",
     title: "Post3",
@@ -52,18 +55,21 @@ const comments = [
     author: "1",
     post: "10",
   },
+
   {
     id: "2",
     text: "comment 2nd",
     author: "1",
     post: "10",
   },
+
   {
     id: "3",
     text: "comment 3rd",
     author: "2",
     post: "20",
   },
+
   {
     id: "4",
     text: "comment 4th",
@@ -79,6 +85,12 @@ const typeDefs = `
       me: User!
       posts(query: String): [Post!]!
       comments: [Comment!]!
+    }
+
+    type Mutation {
+      createUser(name: String!, email: String!, age: Int): User!
+      createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+      createComment(text: String!, author: ID!, post: ID!) : Comment!
     }
 
     type User {
@@ -141,8 +153,8 @@ const resolvers = {
         const isTitleMatch = post.title
           .toLowerCase()
           .includes(args.query.toLowerCase());
-        const isBodyMatch = post.body
 
+        const isBodyMatch = post.body
           .toLowerCase()
           .includes(args.query.toLowerCase());
 
@@ -150,37 +162,96 @@ const resolvers = {
       });
     },
   },
+
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const isEmailTaken = users.some((user) => user.email === args.email);
+
+      if (isEmailTaken) {
+        throw new Error("Email taken.");
+      }
+
+      const user = {
+        id: uuidv4(),
+        ...args
+      };
+
+      users.push(user);
+
+      return user;
+    },
+
+    createPost(parent, args, ctx, info) {
+      const isUserExists = users.some((user) => user.id === args.author);
+
+      if (!isUserExists) {
+        throw new Error("User not found");
+      }
+
+      const post = {
+        id: uuidv4(),
+        ...args
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+
+    createComment(parent, args, ctx, info) {
+      const isPostExistsAndPublished = posts.some((post) => post.id === args.post && post.published);
+      const isUserExists = users.some((user) => user.id === args.author);
+
+      if (!isPostExistsAndPublished && !isUserExists) {
+        throw new Error('User or post not exist!')
+      }
+
+      const comment = {
+        id: uuidv4(),
+        ...args
+      }
+
+      comments.push(comment)
+
+      return comment
+    },
+  },
+
   Post: {
     author(parent, args, ctx, info) {
       return users.find((user) => {
         return user.id === parent.author;
       });
     },
-    comments(parent, args, ctx, info){
-      return comments.filter((comment)=>{
-        return comment.post === parent.id 
-      })
-    }
 
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.post === parent.id;
+      });
+    },
   },
+
   User: {
     posts(parent, args, ctx, info) {
       return posts.filter((post) => {
         return post.author === parent.id;
       });
     },
+
     comments(parent, args, ctx, info) {
       return comments.filter((comment) => {
         return comment.author === parent.id;
       });
     },
   },
+
   Comment: {
     author(parent, args, ctx, info) {
       return users.find((user) => {
-        return user.id === parent.autho;
+        return user.id === parent.author;
       });
     },
+
     post(parent, args, ctx, info) {
       return posts.find((post) => {
         return post.id === parent.post;
