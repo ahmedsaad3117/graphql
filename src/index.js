363ba2af0b045e14fb186d754e67ab1135e08f33
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-09-05 01:56:20 +0200

    add Subscription - Web Socket

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
 import Comment from "./resolvers/Comment";
 import Post from "./resolvers/Post";
 import User from "./resolvers/User";
+import Subscription from "./resolvers/Subscription";
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     Comment,
     Post,
     User,
@@ -18,6 +22,7 @@ const server = new GraphQLServer({
 
   context: {
     db,
+    pubsub,
   },
 });
 
